e77f86cc345e77c03206a283b2f239d0ee1b1218
sachinsingh24 <sachinsingh24@rocketmail.com>
2021-11-20 00:31:49 +0530

    data name changed

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,17 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import Data from '../Data.js';
+import person from '../Data.js';
 import { FaQuoteRight, FaChevronRight, FaChevronLeft } from 'react-icons/fa';
 
 const Review = () => {
   const [index, setIndex] = useState(0);
-  const { name, job, image, text } = Data[index];
+  const { name, job, image, text } = person[index];
   const randomUser = () => {
-    const randomIndex = Math.floor(Math.random() * Data.length);
+    const randomIndex = Math.floor(Math.random() * person.length);
     setIndex(randomIndex);
   };
   useEffect(() => {
     let slide = setInterval(() => {
-      setIndex(index === Data.length - 1 ? 0 : index + 1);
+      setIndex(index === person.length - 1 ? 0 : index + 1);
     }, 1000);
     return () => clearInterval(slide);
   }, [index]);
@@ -27,13 +27,13 @@ const Review = () => {
       <div className='btn-container'>
         <button
           className='prev-btn'
-          onClick={() => setIndex(index === 0 ? Data.length - 1 : index - 1)}
+          onClick={() => setIndex(index === 0 ? person.length - 1 : index - 1)}
         >
           <FaChevronLeft />
         </button>
         <button
           className='next-btn'
-          onClick={() => setIndex(index === Data.length - 1 ? 0 : index + 1)}
+          onClick={() => setIndex(index === person.length - 1 ? 0 : index + 1)}
         >
           <FaChevronRight />
         </button>
